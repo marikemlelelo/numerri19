011426e9fa76565acf6ffc4714ebd09748cae3f9
marikemlelelo <165197146+marikemlelelo@users.noreply.github.com>
2024-04-17 14:08:10 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
     for (let index = 0; index < 10; index++) {
-        let tenpo = 0
-        antera_t += -1
-        basic.showNumber(tenpo)
+        defina += -1
+        basic.showNumber(defina)
         basic.pause(100)
     }
     music.play(music.stringPlayable("E D F G B C5 A B ", 26), music.PlaybackMode.UntilDone)
 })
-let antera_t = 0
-basic.showNumber(10)
-let defina = 10
+let defina = 0
+defina = 10
+basic.showNumber(defina)
 basic.forever(function () {
-    basic.showIcon(IconNames.Sad)
-    basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
+	
 })
